bf84beab751755c7a6056ed962d123ea97752e05
xxj15 <seojini00@naver.com>
2025-05-01 02:57:13 +0900

    1928 (최소힙)

---
+++ BAEKJOON/BOJ_BETA/실버/1927.py
@@ -0,0 +1,16 @@
+# 최소 힙 
+import sys, heapq
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+for _ in range(n):
+    x = int(input())
+    if (x == 0):
+        if (len(heap) == 0):
+            print(0)
+        else :
+            print(heapq.heappop(heap))
+    
+    else:
+        heapq.heappush(heap, x)
