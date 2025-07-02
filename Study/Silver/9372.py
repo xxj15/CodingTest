00ada1bd2217de62e60696aeb7548cc3d3a6219b
xxj15 <seojini00@naver.com>
2025-07-02 17:49:23 +0900

    상근이의 여행(graph)

---
+++ Study/Silver/9372.py
@@ -0,0 +1,12 @@
+# 상근이의 여행
+import sys
+input = sys.stdin.readline
+
+T = int(input()) # 테스트 케이스 수
+
+for _ in range(T): 
+  N, M = map(int, input().split()) # N : 국가의 수, M : 비행기 종류 
+
+  for _ in range(M): 
+        input()
+  print(N-1)
