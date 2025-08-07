e8ca31a2b41e4f03e2f92c665927412c64fe7e51
xxj15 <seojini00@naver.com>
2025-08-08 01:51:40 +0900

    올림픽 (정렬)

---
+++ Study/Silver/8979.py
@@ -0,0 +1,18 @@
+#올림픽
+
+n, k = map(int, input().split())
+rows = []
+for _ in range(n):
+  row = list(map(int, input().split()))
+  rows.append(row)
+
+rows.sort(key=lambda x: [x[1],x[2], x[3]] , reverse=True)
+rate = 1
+
+for i in range(n):
+  if i>0 and rows[i][1:] != rows[i-1][1:]:
+    rate = i + 1
+  
+  if rows[i][0] == k:
+    print(rate)
+    break
