9c431e6a63e6b718fbb3927cc61cb5be1659e884
seojin <63233128+xxj15@users.noreply.github.com>
2025-08-08 01:43:42 +0900

    [Silver V] Title: 올림픽, Time: 88 ms, Memory: 108384 KB, Score: 20 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -7,21 +7,12 @@ for _ in range(n):
   rows.append(row)
 
 rows.sort(key=lambda x: [x[1],x[2], x[3]] , reverse=True)
-pre_gold, pre_silver, pre_bronze = rows[0][1], rows[0][2], rows[0][3]
-rate = 0
+rate = 1
 
-for row in rows : 
-  if row[0] == k:
-    if [row[1], row[2], row[3]] == [pre_gold,pre_silver, pre_bronze]:
-      print(rate)
-      break 
-    else: 
-      rate += 1
-      print(rate)
-      break
-  else:
-    if [row[1], row[2], row[3]] == [pre_gold,pre_silver, pre_bronze]:
-      continue
-    else: 
-      rate += 1
-
+for i in range(n):
+  if i>0 and rows[i][1:] != rows[i-1][1:]:
+    rate += 1
+  
+  if rows[i][0] == k:
+    print(rate)
+    break
