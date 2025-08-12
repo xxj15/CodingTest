c36de8b5112d2f8c947f1ec38561ddee16b28311
xxj15 <seojini00@naver.com>
2025-08-12 23:29:30 +0900

    국영수(정렬)

---
+++ Study/유형별 48문제/Q23(정렬).py
@@ -0,0 +1,12 @@
+# 국영수
+N = int(input())
+
+students=[]
+for _ in range(N):
+    name, kor, eng, math = input().split()
+    students.append((name, int(kor), int(eng), int(math)))
+
+students.sort(key=lambda x : (-x[1], x[2], -x[3], x[0]))
+
+for student in students:
+  print(student[0])
