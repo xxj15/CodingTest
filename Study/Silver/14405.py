a4f3bf2fa253ba60223335e1b2cef4e5ec4e1e2d
xxj15 <seojini00@naver.com>
2025-09-20 16:45:00 +0900

    피카츄(그리디)

---
+++ Study/Silver/14405.py
@@ -0,0 +1,15 @@
+# 피카츄
+S = input()
+idx = 0 
+
+while idx<len(S)-1:
+  if S[idx:idx+2] == 'pi' or S[idx:idx+2] == 'ka':
+    idx = idx+2
+  elif S[idx:idx+3] == 'chu':
+    idx = idx+3 
+  else: 
+    print('NO')
+    break
+
+else: # while 문 돌고 else 블록 실행됨 
+    print("YES")
