66aee7563466a581e9d528307f6dd602b10f5ca1
seojin <63233128+xxj15@users.noreply.github.com>
2025-10-02 16:12:47 +0900

    [Silver IV] Title: 오셀로 재배치, Time: 328 ms, Memory: 115932 KB -BaekjoonHub

---
+++ 백준/Silver/13413. 오셀로 재배치/오셀로 재배치.py
@@ -0,0 +1,23 @@
+# 오셀로 재배치
+import math
+T = int(input())
+
+def check(first_str, goal_str):
+  to_white=0
+  to_black=0
+
+  for f, g in zip(first_str, goal_str):
+    if f != g:
+      if f == 'B' and g == 'W':
+        to_white+=1
+      else:
+        to_black+= 1
+  return max(to_black, to_white)
+    
+
+for _ in range(T):
+  N = int(input())
+  first =input()
+  goal =input()
+  print(check(first, goal))
+
