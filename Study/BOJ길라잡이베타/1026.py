9c0830c21e226652402178b787e754ea237a645f
xxj15 <seojini00@naver.com>
2025-10-20 22:21:21 +0900

    보물(실4) - Greedy

---
+++ Study/BOJ길라잡이베타/1026.py
@@ -0,0 +1,13 @@
+# 보물(실4) - Greedy
+N = int(input())
+
+listA = list(map(int, input().split()))
+listB = list(map(int, input().split()))
+
+listA.sort()
+listB.sort(reverse=True)
+ans = 0 
+for i in range(N):
+  ans += listA[i]*listB[i]
+
+print
