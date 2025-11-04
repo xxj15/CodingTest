284f75da5ab5b3a3cb6a574fc4c86874c4fa2c3c
seojin <63233128+xxj15@users.noreply.github.com>
2025-11-04 19:58:35 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 4.52 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* ineq, const char* eq, int n, int m) {
+    int answer = 0;
+    
+    if (strcmp(ineq,"<") == 0){
+        if (strcmp(eq, "=") == 0){
+            if (n<=m){
+                answer = 1;
+            }else{
+                answer = 0;
+            }
+        }else{
+            if (n<m){
+                answer=1;
+            }
+            else{
+                answer=0;
+            }
+        }
+    }else{
+        if (strcmp(eq, "=") == 0){
+            if(n>=m){
+                answer=1;
+            }else{
+                answer=0;
+            }
+        }else{
+            if(n>m){
+                answer =1;
+            }else{
+                answer=0;
+            }
+        }
+    }
+    
+    return answer;
+}
