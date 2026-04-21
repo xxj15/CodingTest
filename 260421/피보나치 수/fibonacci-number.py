73e9000f087ce891c500a6adbbabba152797c607
seojin <63233128+xxj15@users.noreply.github.com>
2026-04-21 15:38:22 +0900

    Title: 피보나치 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260421/피보나치 수/fibonacci-number.py
+++ 260421/피보나치 수/fibonacci-number.py
@@ -1,11 +1,14 @@
 N = int(input())
 
-dp = [-1]*(N+1)
+if N == 1:
+    print(1)
+else:
+    dp = [-1]*(N+1)
 
-dp[1]=1
-dp[2]=1
+    dp[1]=1
+    dp[2]=1
 
-if N>2:
+
     for i in range(3, N+1):
         dp[i] = dp[i-1]+dp[i-2]
-print(dp[N])
+    print(dp[N])
